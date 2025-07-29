ba1842c60176044ac668a88916ef3bd8eff08b2c
heitorfonsatti <125671020+heitorfonsatti@users.noreply.github.com>
2025-07-29 15:52:44 -0300

    Classe TransactionRecordDTO criada

---
+++ src/main/java/com/heitor/Bank/Transaction/DTO/TransactionRecordDTO.java
@@ -0,0 +1,10 @@
+package com.heitor.Bank.Transaction.DTO;
+
+import com.heitor.Bank.Transaction.Model.TransactionType;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public record TransactionRecordDTO(@NotNull TransactionType type, @NotNull BigDecimal amount, UUID sourceAccountId, UUID destinationAccountId) {
+}
