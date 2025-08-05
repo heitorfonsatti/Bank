90d04d5814b925f2d0bc08f3b10e2badb0fc4d7f
heitorfonsatti <125671020+heitorfonsatti@users.noreply.github.com>
2025-08-05 15:19:22 -0300

    Ajustes na classe BankAccountModel

--- src/main/java/com/heitor/Bank/BankAccount/Model/BankAccountModel.java
+++ src/main/java/com/heitor/Bank/BankAccount/Model/BankAccountModel.java
@@ -1,5 +1,9 @@
 package com.heitor.Bank.BankAccount.Model;
 
+import com.heitor.Bank.BankAccount.Utils.BankAccountType;
+import com.heitor.Bank.Card.Model.CardModel;
+import com.heitor.Bank.Client.Model.ClientModel;
+import com.heitor.Bank.Transaction.Model.TransactionModel;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,6 +16,8 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -27,14 +33,28 @@ public class BankAccountModel extends RepresentationModel<BankAccountModel> impl
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID idAccount;
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private int accountNumber;
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private int agencyNumber;
-    private String type;
+
+    @Enumerated(EnumType.STRING)
+    private BankAccountType type;
     private BigDecimal balance;
+
     @CreationTimestamp
     @Column(updatable = false)
     private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id", nullable = false)
+    private ClientModel client;
+
+    @OneToMany(mappedBy = "bankAccount", cascade = CascadeType.ALL)
+    private List<CardModel> cards = new ArrayList<>();
+
+    @OneToMany(mappedBy = "sourceAccount")
+    private List<TransactionModel> sentTransactions = new ArrayList<>();
+
+    @OneToMany(mappedBy = "destinationAccount")
+    private List<TransactionModel> receivedTransactions = new ArrayList<>();
 
 }
