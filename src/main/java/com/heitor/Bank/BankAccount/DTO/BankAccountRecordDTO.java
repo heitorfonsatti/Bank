a19f073cc3805279bb8cf9d44e38673a0d2a733b
heitorfonsatti <125671020+heitorfonsatti@users.noreply.github.com>
2025-08-05 15:19:49 -0300

    Ajustes no Record BankAccountRecordDTO

--- src/main/java/com/heitor/Bank/BankAccount/DTO/BankAccountRecordDTO.java
+++ src/main/java/com/heitor/Bank/BankAccount/DTO/BankAccountRecordDTO.java
@@ -1,10 +1,12 @@
 package com.heitor.Bank.BankAccount.DTO;
 
+import com.heitor.Bank.BankAccount.Utils.BankAccountType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.UUID;
 
-public record BankAccountRecordDTO(@NotNull BigDecimal balance, @NotBlank String type, @NotNull LocalDate createdAt) {
+public record BankAccountRecordDTO(@NotNull BigDecimal balance, @NotBlank BankAccountType type, @NotNull LocalDate createdAt, @NotNull UUID clientId) {
 }
