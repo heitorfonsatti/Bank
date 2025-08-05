e1b59fbc0f1704c01eb769ac49a7c210c9b3891b
heitorfonsatti <125671020+heitorfonsatti@users.noreply.github.com>
2025-08-05 15:16:16 -0300

    Classe UserModel criada

---
+++ src/main/java/com/heitor/Bank/User/Model/UserModel.java
@@ -0,0 +1,36 @@
+package com.heitor.Bank.User.Model;
+
+import com.heitor.Bank.User.Utils.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_user")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserModel extends RepresentationModel<UserModel> implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID userId;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
